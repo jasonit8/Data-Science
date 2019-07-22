f6d5ca3fa37b588a3c852ee22c9d247f54a209a5
jasonit8 <52004170+jasonit8@users.noreply.github.com>
2019-07-22 14:48:04 +0700

    Update symtriangles.py

--- Python/symtriangles.py
+++ Python/symtriangles.py
@@ -1,4 +1,5 @@
-n = int(input('Độ dài cạnh tam giác: '))
+# Draw 2 symmetric isosceles right triangles with respect to a vertex.
+n = int(input('Length of one edge: '))
 s = ['*']+['']*(n-2)+['* '*(2*n-1)]+['']*(n-2) + ['  '*(2*n-2)+'*']
 l = len(s)
 for i in range(1,n-1):
